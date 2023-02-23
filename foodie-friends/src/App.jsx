85f25c98d47c8cfd683b608c14bb2bada524eff5
Truc Duong <trucdg@bu.edu>
2023-02-23 17:01:51 -0500

    create a functional React component for the Restaurant component and import it into the App Component with some test codes

--- foodie-friends/src/App.jsx
+++ foodie-friends/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Restaurant from "./components/Restaurant";
 
 function App() {
   return (
     <div>
       <h1>Foodie Friends</h1>
+      <Restaurant />
     </div>
   );
 }
---
+++ foodie-friends/src/components/Restaurant.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Restaurant = () => {
+  return (
+    <div>
+      <h1> this is a restaurant component</h1>
+    </div>
+  );
+};
+
+export default Restaurant;
