e281b4821130202226fb7c72cad0c5462ca6858f
Truc Duong <trucdg@bu.edu>
2023-02-23 17:13:13 -0500

    add basic element into the Restaurant component

--- foodie-friends/src/components/Restaurant.jsx
+++ foodie-friends/src/components/Restaurant.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const Restaurant = () => {
   return (
     <div>
-      <h1> this is a restaurant component</h1>
+      <div>
+        <img
+          src="https://s3-media0.fl.yelpcdn.com/bphoto/4Na6YdEKTo1MJrHCsHLd4A/o.jpg"
+          alt="displayed food"
+        ></img>
+      </div>
+      <div>
+        <h2> Fogo de Chao Steakhouse </h2>
+        <p>Mexican</p>
+      </div>
     </div>
   );
 };
