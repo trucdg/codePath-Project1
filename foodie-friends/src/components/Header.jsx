2e1c18aff3e6dcc7eb8fb530b8ad4ed3fdd88b90
Truc Duong <trucdg@bu.edu>
2023-02-23 20:05:33 -0500

    make the description in the header only visible on large screen size

--- foodie-friends/src/components/Header.jsx
+++ foodie-friends/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <div class="overlay">
         <h1>Foodie Friends</h1>
         <h5>Good Food, Good Life</h5>
-        <p>
+        <p class="d-none d-lg-block">
           There are many great restaurants and coffee shops in the Boston area,
           but that doesn't means that many students know about them. This web
           app is a resource board that displays some popular restaurants around
